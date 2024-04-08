92391845785b58a368ffa7be91ed7cb165b661cf
Akshay Meher <meherakshay59@gmail.com>
2024-04-08 12:14:27 +0530

    Session-4 Tasks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import "./App.css"
+import Counter from "../src/components/L1_LifeCycle_Class_Comp/Ass1/Counter"
+import Session4 from './components/L4_Props_Drilling_UseState/Session4';
 // import ParentCard from './components/L3_children_callBack/Child_to_parent/ParentCard';
-import LearnUseState from './components/L3_children_callBack/useState/LearnUseState';
+// import LearnUseState from './components/L3_children_callBack/useState/LearnUseState';
 // import Test from './components/1_LifeCycle/Test';
 // import Counter from './components/Counter';
 // import Task2 from './components/1_LifeCycle/Task2';
@@ -25,7 +27,17 @@ function App() {
       {/* <ParentCard /> */}
 
       {/* Ass-D3.2: Store html-tags, Images, Logoes, Nested Objects, Arrays*/}
-      <LearnUseState />
+      {/* <LearnUseState /> */}
+
+      {/*
+          Ass-D4.1: Img src Error Handling
+          Ass-D4.2: Learn Console Object Methods 
+          Ass-D4.3: Implement Conditional Counter  
+          Ass-D4.4: Props drilling with Multilevel Child Comp 
+          Ass-D4.5: Adding Two Array Using Spread operator  
+          Ass-D4.6: Manupulate Arrays (add, update, delete)  
+      */}
+      <Session4 />
 
     </div>
   )
